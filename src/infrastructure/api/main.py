c90cfd78d66be1bcdf12f3e5611d4774c4693a0a
Andres Pimentel <pimentelandressantiago@gmail.com>
2026-07-23 13:13:33 -0500

    final

--- src/infrastructure/api/main.py
+++ src/infrastructure/api/main.py
@@ -1,14 +1,6 @@
 from fastapi import FastAPI
+from src.infrastructure.metrics import metrics
 from src.infrastructure.api.routes import router
 from src.infrastructure.middlrewares.middlrewares import metrics_middleware, log_request_middleware 
 app = FastAPI(title="prediction models via API")
 app.include_router(router=router)
-@app.middleware("http")
-async def metrics_middleware(request, call_next):
-    # tu lógica aquí
-    return await call_next(request)
-
-@app.middleware("http")
-async def log_request_middleware(request, call_next):
-    # tu lógica aquí
-    return await call_next(request)
